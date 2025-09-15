672a7ed05aedbe1b2df69870c6199c8a3bec26af
Hardik Panchal <hardik.pan@befree.local>
2025-09-15 11:09:44 +0530

    chages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 </head>
 <body class="bg-gray-100 flex items-center justify-center h-screen">
     <div class="text-center">
-        <h1 class="text-4xl font-bold text-blue-600">🚀 Welcome to My Laravel Demo App - Hardik befree</h1>
+        <h1 class="text-4xl font-bold text-blue-600">🚀 Welcome to My Laravel Demo App - Hardik befree IT</h1>
         <p class="mt-4 text-gray-700">This is a simple welcome page created in Laravel.</p>
         <a href="https://laravel.com" target="_blank" class="mt-6 inline-block px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
             Learn Laravel
