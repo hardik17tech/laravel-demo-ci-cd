b86dee4553537a711863004d2abfb12e4b912379
Hardik Panchal <hardik.pan@befree.local>
2025-09-15 11:39:56 +0530

    ready for deploy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
 <body class="bg-gray-100 flex items-center justify-center h-screen">
     <div class="text-center">
         <h1 class="text-4xl font-bold text-blue-600">🚀 Welcome to My Laravel Demo App - Hardik befree IT inf</h1>
-        <p class="mt-4 text-gray-700">This is a simple welcome page created in Laravel.</p>
+        <p class="mt-4 text-gray-700">This is a simple welcome page created in Laravel. for testing </p>
         <a href="https://laravel.com" target="_blank" class="mt-6 inline-block px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
             Learn Laravel
         </a>
