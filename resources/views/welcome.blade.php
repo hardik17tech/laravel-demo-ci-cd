64d3c7d8dc673d190e5932a4639507332fe3b1df
Hardik Panchal <hardik.pan@befree.local>
2025-09-19 14:37:51 +0530

    changes done - 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
         <h1 class="text-4xl font-bold text-blue-600">🚀 Welcome to My Laravel Demo App - Hardik befree CSCpvt ltd on stagging server live 123</h1>
         <p class="mt-4 text-gray-700">This is welcome page </p>
         <a href="https://laravel.com" target="_blank" class="mt-6 inline-block px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
-            Learn Laravel 123456 - staggin file added and deploy on stagging_1.1.2 -  changes - wip-hardik 1  Hp1711 - 1111
+            Learn Laravel 123456 - staggin file added and deploy on stagging_1.1.2 -  changes - wip-hardik 1  Hp1711 - 1111 -- changes done
         </a>
     </div>
 </body>
