39efd3eb01167b94e374ef6a2b71ccdfad266968
hardik17tech <hardik.pan@befree.com.au>
2026-02-18 14:11:09 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\WelcomeController;
 
 
 Route::get('/', function () {
-    return view('welcome to Befree 9867 123 234');
+    return view('welcome to Befree 9867 123 234 123');
 });
 
 Route::get('/welcome', [WelcomeController::class, 'index']);
